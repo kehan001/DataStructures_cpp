1a3a1e9fb2e4fd5df60b21b47cf84d659e6ac07b
kehan001 <kqvp0474@leeds.ac.uk>
2026-04-06 17:58:59 +0100

    2026-4-6

--- DataStructures/TopologicalSort.cpp
+++ DataStructures/TopologicalSort.cpp
@@ -1,3 +1,60 @@
+//Test case
+
+
+//const int vertexNum = 14;
+//const int edgeNum = 20;
+//
+//vector<vector<int>> G(vertexNum, vector<int>(vertexNum, 0));
+//
+//G[0][4] = 1;
+//G[0][5] = 1;
+//G[0][11] = 1;
+//
+//G[1][2] = 1;
+//G[1][4] = 1;
+//G[1][8] = 1;
+//
+//G[2][5] = 1;
+//G[2][6] = 1;
+//G[2][9] = 1;
+//
+//G[3][2] = 1;
+//G[3][13] = 1;
+//
+//G[4][7] = 1;
+//
+//G[5][8] = 1;
+//G[5][12] = 1;
+//
+//G[6][5] = 1;
+//
+//G[8][7] = 1;
+//
+//G[9][10] = 1;
+//G[9][11] = 1;
+//
+//G[10][13] = 1;
+//
+//G[12][9] = 1;
+//
+//
+//vector<int> indegree(vertexNum, 0);
+//vector<vector<int>> adjList(vertexNum);
+//
+//for (int i = 0; i < vertexNum; i++) {
+//    for (int j = 0; j < vertexNum; j++) {
+//        if (G[i][j] == 1) {
+//            adjList[i].push_back(j);
+//            indegree[j] += 1;
+//        }
+//
+//    }
+//}
+//
+//
+//TopologicalSort(indegree, adjList);
+
+
 #include "TopologicalSort.h"
 
 void TopologicalSort(std::vector<int> indegree, const std::vector<std::vector<int>>& adjList) {
